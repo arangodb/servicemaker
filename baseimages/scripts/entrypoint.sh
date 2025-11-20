b873452571d986aafc30fa71868f52d9b9c77ab2
Max Neunhoeffer <max@arangodb.com>
2025-11-20 11:36:43 +0100

    Fixing zip execution.

--- baseimages/scripts/entrypoint.sh
+++ baseimages/scripts/entrypoint.sh
@@ -4,18 +4,25 @@ cd /project
 
 # Try to download project zip, if the `projectURL` file is there:
 if test -e projectURL ; then
+  echo Downloading project.tar.gz ...
   curl -O project.tar.gz $(cat projectURL)
 fi
 
 # Try to unzip the project zip, if the `project.tar.gz` file is there:
 if test -e project.tar.gz ; then
-  tar xzvf project.tar.gz
+  echo Extracting project.tar.gz ...
+  tar xzvf project.tar.gz > /dev/null
 fi
 
 # Run the entrypoint if configured:
 if test -L entrypoint ; then
-  export PYTHONPATH=/project
-  exec entrypoint
+  echo Running project ...
+  . /home/user/.local/bin/env
+  . /home/user/the_venv/bin/activate
+  for p in /project/the_venv/lib/python*/site-packages ; do
+    export PYTHONPATH=$p
+  done
+  exec uv run ./entrypoint
 fi
 
 echo No entrypoint found, running bash instead...
