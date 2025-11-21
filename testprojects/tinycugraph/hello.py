adfd5e1b3985041bc87edb5bd3c27491892a6f27
Max Neunhoeffer <max@arangodb.com>
2025-11-21 15:21:11 +0100

    New set of base images.

---
+++ testprojects/tinycugraph/hello.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Simple hello world program using colorama package."""
+
+from colorama import Fore, Style, init
+import cugraph
+
+# Initialize colorama
+init(autoreset=True)
+
+def main():
+    print(f"{Fore.GREEN}Hello World!{Style.RESET_ALL}")
+    print("Cugraph version:", cugraph.__version__)
+
+if __name__ == "__main__":
+    main()
--- testprojects/tinytorch/hello.py
+++ testprojects/tinytorch/hello.py
@@ -2,12 +2,14 @@
 """Simple hello world program using colorama package."""
 
 from colorama import Fore, Style, init
+import pytorch
 
 # Initialize colorama
 init(autoreset=True)
 
 def main():
     print(f"{Fore.GREEN}Hello World!{Style.RESET_ALL}")
+    print("Pytorch version:", pytorch.__version__)
 
 if __name__ == "__main__":
     main()
