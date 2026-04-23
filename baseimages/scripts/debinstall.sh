63639c716cc1fbfbbd3401982a6f4eae1411bcd3
bluepal-yaswanth-peravali <yaswanth.peravali@bluepal.com>
2026-04-23 15:07:48 +0530

    modifications in scripts to fix security scan failure

--- baseimages/scripts/debinstall.sh
+++ baseimages/scripts/debinstall.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-# To be run as root to prepare a bland debian base image
+# To be run as root to prepare base image
 apt-get update
 apt-get upgrade -y
-apt-get install -y curl adduser bash
+
+apt-get install -y --no-install-recommends \
+    curl \
+    adduser \
+    bash \
+    python3 \
+    python3-venv \
+    python3-pip
+
 apt-get clean
+rm -rf /var/lib/apt/lists/*
+
 adduser user
 mkdir /project
 chown -R user:user /project
--- baseimages/scripts/prepareuv.sh
+++ baseimages/scripts/prepareuv.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # To be run as user `user` in /home/user to prepare uv and `the_venv`
 
-PYTHON_VERSION=$1
 shift
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
@@ -9,7 +8,8 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 export UV_HTTP_TIMEOUT=3600
 rm -rf the_venv
 . /home/user/.local/bin/env
-uv venv --python $PYTHON_VERSION the_venv
+# Use system Python instead of custom version
+python3 -m venv the_venv
 . the_venv/bin/activate
 uv pip install "$@"
 
